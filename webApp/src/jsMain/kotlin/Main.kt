becc25dd5a7adec3655e1ab221a6a13a1ba0d421
Abdul Hakeem <ahm.mhd78@gmail.com>
2023-04-29 10:15:50 +0400

    webApp added

---
+++ webApp/src/jsMain/kotlin/Main.kt
@@ -0,0 +1,14 @@
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
+import org.jetbrains.skiko.wasm.onWasmReady
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    onWasmReady {
+        CanvasBasedWindow {
+            App()
+        }
+    }
+}
+
---
+++ webApp/src/wasmMain/kotlin/Main.wasm.kt
@@ -0,0 +1,11 @@
+import androidx.compose.runtime.*
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.CanvasBasedWindow
+import kotlin.wasm.unsafe.*
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    CanvasBasedWindow {
+        App()
+    }
+}
